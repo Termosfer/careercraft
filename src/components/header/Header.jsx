6005c09d6fc53abd4da654f2635be024a56862a5
Toghrul Aghayev <adadsad@gmail.com>
2024-08-08 01:47:58 +0400

    new update abot user dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,20 @@
 import "./style.css";
 import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 import logo from "../../assets/Logo.png";
 import Image from "react-bootstrap/Image";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userData, logout } from "../../config/authLogin";
 function CollapsibleExample() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.login.user);
-  const [active, setActive] = useState(false);
   console.log(user, "asd");
   useEffect(() => {
     dispatch(userData());
   }, [dispatch]);
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container className="d-flex align-items-center justify-content-center justify-content-md-between w-75 px-md-5 px-3">
@@ -48,21 +47,25 @@ function CollapsibleExample() {
           </Nav>
           <Nav className="d-flex justify-content-md-between justify-content-between align-items-md-center align-items-center column-gap-2 mt-2 mt-lg-0 text-nowrap">
             {user ? (
-              <div>
-                <Link to="/auth/register" className="me-1 sign-up">
-                  {user.name}
-                </Link>
-                <Link
-                  to="/auth/register"
-                  className="me-1 login"
-                  onClick={() => dispatch(logout())}
-                >
-                  Logout
-                </Link>
+              <>
+                <Dropdown>
+                  <Dropdown.Toggle className="sign-up" id="dropdown-basic">
+                    {user.name}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item to="/user/profile">
+                      Profile
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={()=>dispatch(logout())}>
+                      Logout
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
                 <Link to="/auth/login" className="ms-1 login">
                   Book a Demo
                 </Link>
-              </div>
+              </>
             ) : (
               <div>
                 <Link to="/auth/register" className="me-1 sign-up">
