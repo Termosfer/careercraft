536e67f380c9ca915591ec04c3ad48fd5eb55278
Toghrul <adadsa>
2024-08-16 13:47:23 +0400

    new update about answer

--- src/config/getQuestions.js
+++ src/config/getQuestions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const initialState = {
   question: [],
+  answer: [],
   loading: false,
   error: "",
   value: 0,
@@ -11,12 +12,36 @@ const initialState = {
 export const getQuestion = createAsyncThunk(
   "questions/getQuestion",
   async (count) => {
-    const response = await axios.get(
-      `http://44.203.152.52:8080/questions/grouped-by-skills?skillIds=1&jobId=1&id=${count}`
-    );
-    return response.data.skillsQuestions;
+    const token = localStorage.getItem("token")
+    if (token) {
+      const response = await axios.get(
+        `http://44.203.152.52:8070/questions/grouped-by-skills?skillIds=1&jobId=1&id=${count}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      }
+      );
+      return response.data.skillsQuestions;
+    }
   }
+
 );
+
+
+export const getAnswer = createAsyncThunk("questions/getAnswer", async () => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    const response = await axios.post("http://44.203.152.52:8070/user-answers/answer",{}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+    return response.data
+  }
+
+})
+
+
 
 const questionsSlice = createSlice({
   name: "questions",
@@ -39,9 +64,21 @@ const questionsSlice = createSlice({
       .addCase(getQuestion.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      .addCase(getAnswer.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(getAnswer.fulfilled, (state, action) => {
+        state.loading = false;
+        state.answer = action.payload;
+      })
+      .addCase(getAnswer.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
       });
   },
 });
-export const {changeIncrease} = questionsSlice.actions;
+export const { changeIncrease } = questionsSlice.actions;
 
 export default questionsSlice.reducer;
--- src/pages/BookaDemo/FreeTrial.jsx
+++ src/pages/BookaDemo/FreeTrial.jsx
@@ -1,32 +1,34 @@
-// src/components/FreeTrial.jsx
+
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getQuestion } from "../../config/getQuestions";
+import { getAnswer, getQuestion } from "../../config/getQuestions";
 import { Container, Form, Button } from "react-bootstrap";
 
 import { changeIncrease } from "../../config/getQuestions";
 const FreeTrial = () => {
   const dispatch = useDispatch();
-  const count = useSelector((state)=>state.questions.value)
-  console.log(count, "count")
-  const { question, loading, error } = useSelector((state) => state.questions);
+  const count = useSelector((state) => state.questions.value)
+
+  const { question, loading, error, answer } = useSelector((state) => state.questions);
   console.log(question, "quest");
+console.log(answer, "ans")
   useEffect(() => {
-    
-    dispatch(getQuestion(count)); 
+
+    dispatch(getQuestion(count));
+    dispatch(getAnswer())
   }, [dispatch, count]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-const submitHandler = (e)=>{
-  e.preventDefault()
-  dispatch(changeIncrease())
-}
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(changeIncrease())
+  }
 
   return (
     <Container className="w-75 px-5 py-4 mt-5 border rounded">
-      {question?.map((quest,index) => (
+      {question?.map((quest, index) => (
         <div key={index}>
           <h1>{quest.skillName}</h1>
           {quest.questions?.map((n) => (
@@ -35,7 +37,7 @@ const submitHandler = (e)=>{
               <Form onSubmit={submitHandler}>
                 {n.answers?.map((answer) => (
                   <ul key={answer.id} className="list-unstyled">
-                    
+
                     <li className="d-flex gap-2"><input type="radio" name="name" required />  {answer.text}</li>
                   </ul>
                 ))}
@@ -51,3 +53,7 @@ const submitHandler = (e)=>{
 };
 
 export default FreeTrial;
+
+
+
+
