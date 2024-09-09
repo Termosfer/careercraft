8e643aec73bbc2eebedb648b482f102533dffe98
Toghrul <adadsa>
2024-09-09 16:53:17 +0400

    new update  short version of the input in reset pass

--- src/pages/ResetPass/ResetPass.jsx
+++ src/pages/ResetPass/ResetPass.jsx
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   changeConfirmPass,
   changeNewPass,
-  changeResetToken,
 } from "../../config/authSlice";
 import { resetPass } from "../../config/authLogin";
 const ResetPass = () => {
@@ -19,8 +18,8 @@ const ResetPass = () => {
   const [input4, setInput4] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const newPass = useSelector((state) => state.auth.newPass);
-  const confirmPass = useSelector((state) => state.auth.confirmPass);
+  const newPassword = useSelector((state) => state.auth.newPass);
+  const confirmPassword = useSelector((state) => state.auth.confirmPass);
 
   // console.log(input1, "asd");
   // console.log(input2, "asd");
@@ -65,11 +64,11 @@ const ResetPass = () => {
   const handleReset = async (e) => {
     e.preventDefault();
     // console.log("resetted");
-    const allInput = `${input1}${input2}${input3}${input4}`;
+    const token = `${input1}${input2}${input3}${input4}`;
     const reset = {
-      newPassword: newPass,
-      confirmPassword: confirmPass,
-      token: allInput,
+      newPassword,
+      confirmPassword,
+      token,
     };
     dispatch(resetPass(reset));
     const resultAction = await dispatch(resetPass(reset));
@@ -118,7 +117,7 @@ const ResetPass = () => {
                 </div>
               </Form.Group>
 
-              <Form.Group className=" pass-form-part" controlId="formPassword">
+              <Form.Group className=" pass-form-part" controlId="formPassword1">
                 <Form.Label className="form-label-pass d-flex justify-content-between align-items-center">
                   <span>Confirm New Password</span>
                 </Form.Label>
@@ -154,7 +153,6 @@ const ResetPass = () => {
                     height: "50px",
                   }}
                   type="text"
-                  placeholder=""
                   autoFocus
                   maxLength={1}
                   onChange={(e) => {
@@ -173,7 +171,6 @@ const ResetPass = () => {
                     height: "50px",
                   }}
                   type="text"
-                  placeholder=""
                   autoFocus
                   maxLength={1}
                   onChange={(e) => {
