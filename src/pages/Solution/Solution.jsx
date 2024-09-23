26da6840f157ef660c86601b9ca0c30f3a2fb1fb
Toghrul <adadsa>
2024-09-23 14:54:58 +0400

    adas

--- src/pages/Solution/Solution.jsx
+++ src/pages/Solution/Solution.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Solution = () => {
   return (
-    <div>Solution</div>
+    <div className='bg-solutions'>
+
+    <Container>
+
+    </Container>
+    </div>
   )
 }
 
