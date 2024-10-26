08870e68f6070fd94216fc8887873a3c688f3988
AbbasMemmedov <mammadovabbas.0@gmail.com>
2024-10-26 19:27:51 +0400

    the end

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import PublicRoutes from "./routes/PublicRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
-import  { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <BrowserRouter>
       <PublicRoutes />
-      <Toaster position="top-center" reverseOrder={false}/>
+      <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
   );
 }
